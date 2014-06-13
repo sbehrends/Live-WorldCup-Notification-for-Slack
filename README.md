92f0802035618b9da80df9c99a07e1c84af191f8
Sergio Behrends <sergio@aerolab.com.ar>
2014-06-12 22:38:30 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 Live-WorldCup-Notification-for-Slack
 ====================================
+
+It's a NodeJS worker that request every 5 seconds data from the FIFA World Cup API and notifies via Slack API (It's possible to apply other ways of notification) the changes of the match.
+
+====================================
+
++ It notifies when a match starts
++ It notifies when the score changes (this means Goal or the match finished)
+
+It is posible to run it as a worker on Heroku :)
---
+++ bot.js
@@ -0,0 +1,73 @@
+var requestify = require("requestify");
+var async = require("async");
+
+// Create an incoming webhook
+var slack = require('slack-notify')('https://domain.slack.com/services/hooks/incoming-webhook?token=');
+
+
+var matchID = "",
+	matchScore = "",
+	match;
+
+
+var cron = require('cron');
+var cronJob = cron.job("*/5 * * * * *", function(){
+
+
+	// Get Match list
+	requestify.get('http://live.mobileapp.fifa.com/api/wc/matches').then(function(response) {
+	    var matches = response.getBody().data.group;
+
+	    async.filter(matches, function(item, callback) {
+	        callback (item.b_Live == true);
+
+	    }, function(results){
+
+            match = results[0];
+
+            if (typeof match == "object") {
+            	// Got Live Match!
+
+            	if (match.n_MatchID != matchID) {
+            		// New Match just started
+
+            		matchID = match.n_MatchID;
+            		matchScore = ''
+
+            		// Notify New match
+            		var text = 'Comienza '+match.c_HomeTeam_es+ ' vs '+match.c_AwayTeam_es;
+            		console.log(text)
+            		slack.send({
+					  channel: '#futbol',
+					  text: text,
+					  username: 'AeroBot'
+					});
+
+            		
+            	} else if (matchScore != match.c_Score) {
+            		// Different Score
+
+            		matchScore = match.c_Score
+
+            		var text = match.c_HomeTeam_es+ ' '+match.c_Score+' '+match.c_AwayTeam_es+' ';
+
+            		// Notify goal
+            		console.log(text)
+
+            		slack.send({
+					  channel: '#futbol',
+					  text: text,
+					  username: 'AeroBot'
+					});
+
+            	}
+
+            }
+
+
+
+	    });
+
+	});
+}); 
+cronJob.start();
